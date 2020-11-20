5953be7723f2b5992450c072ba0e34813b20a84f
Cobalt <chaosthe0rie@protonmail.com>
2020-11-20 16:27:05 +0100

    add placeholder for games

--- server/logic/js/game.js
+++ server/logic/js/game.js
@@ -1,8 +1,6 @@
 import { PentaMath } from "./pentagame.js";
 import { SVG } from "@svgdotjs/svg.js";
 import { getJSONP, download } from "./core.js";
-let shift = false;
-let data = null;
 
 document.addEventListener("DOMContentLoaded", function () {
   const size = 1000;
@@ -17,53 +15,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   drawer.data({ size: size });
   const math = new PentaMath(drawer);
-  let board = math.draw(drawer, size);
-  /*
-  // action btns
-  const capture_btn = document.getElementById("btn-capture");
-  capture_btn.onclick = (evt) => {
-    download("pentagame.svg", drawer.svg());
-  };
+  let board = math.draw(drawer, size, { shift: true });
 
-  const shift_btn = document.getElementById("btn-shift");
-  shift_btn.onclick = (evt) => {
-    while (drawer.node.lastChild) {
-      drawer.node.removeChild(drawer.node.lastChild);
-    }
-    if (shift === true) {
-      let board = math.draw(drawer, size);
-      shift = false;
-    } else {
-      let board = math.draw(drawer, size, { shift: false });
-      shift = true;
-    }
-  };
-
-  const export_btn = document.getElementById("btn-export");
-  export_btn.onclick = (evt) => {
-    if (data !== null) {
-      download("pentagame.json", JSON.stringify(data));
-    } else {
-      console.log("Data seems not loaded");
-    }
-  };
-
-  const import_modal = document.getElementById("modal-import-form");
-  import_modal.onsubmit = (evt) => {
-    evt.preventDefault();
-    var file = document.getElementById("modal-file-input").files;
-    var reader = new FileReader();
-    reader.onload = function (evt) {
-      data = JSON.parse(evt.target.result);
-      console.log("Data updated!");
-    };
-    reader.readAsText(file[0]);
-  };
-
-  var modals = document.querySelectorAll(".modal");
-  var instances = M.Modal.init(modals);
-
-*/
   Array.prototype.slice
     .call(document.querySelectorAll("[data-id]"))
     .map(function (element) {
